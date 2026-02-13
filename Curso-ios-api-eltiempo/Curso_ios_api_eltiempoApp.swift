2d30022ac6de579a41a10840233f4ca9d81ad64c
Fernando <dwk262@gmail.com>
2026-02-13 12:29:58 +0100

    Initial Commit

---
+++ Curso-ios-api-eltiempo/Curso_ios_api_eltiempoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Curso_ios_api_eltiempoApp.swift
+//  Curso-ios-api-eltiempo
+//
+//  Created by Equipo 9 on 13/2/26.
+//
+
+import SwiftUI
+
+@main
+struct Curso_ios_api_eltiempoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
